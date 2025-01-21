f3c8d277052cb265113ad5563bbf4d02de04682e
sunfeilong <sunfeilong1993@gmail.com>
2025-01-21 15:57:57 +0800

    update

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,21 +1,27 @@
-fn main() {
-    let words = "Hello World";
-
-    let word: &str = first_word(&words);
-    println!("The first word is: {}", word);
+#[derive(Debug)]
+struct User {
+    username: String,
+    age: u16,
+    email: String,
 }
 
-fn first_word(s: &str) -> &str {
-    if s.len() == 0 {
-        return s;
-    }
+fn main() {
+    let mut user1 = User {
+        age: 23,
+        email: String::from("example@www.com"),
+        username: String::from(""),
+    };
+    user1.username = String::from("王五");
+    user1.email = String::from("anotheremail@example.com");
+    user1.age = 24;
 
-    let mut index = s.len();
-    for (i, &item) in s.as_bytes().iter().enumerate() {
-        if item == b' ' {
-            index = i;
-        }
-    }
 
-    return &s[0..index];
+    let user2 = User {
+        username: String::from("李四"),
+        age: 23,
+        email: String::from("anotheremail@example.com"),
+    };
+
+    println!("The value of user1 is: {:?}", user1);
+    println!("The value of user2 is: {:?}", user2);
 }
