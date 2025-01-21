f9016488d89eee6f9133c0bdf387298c4b9e6536
sunfeilong <sunfeilong1993@gmail.com>
2025-01-21 15:30:50 +0800

    update

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world! ");
+    let words = "Hello World";
+
+    let word: &str = first_word(&words);
+    println!("The first word is: {}", word);
+}
+
+fn first_word(s: &str) -> &str {
+    if s.len() == 0 {
+        return s;
+    }
+
+    let mut index = s.len();
+    for (i, &item) in s.as_bytes().iter().enumerate() {
+        if item == b' ' {
+            index = i;
+        }
+    }
+
+    return &s[0..index];
 }
